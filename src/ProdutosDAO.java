4eaa91846e52817f34b90a39dbaf64ff024e91aa
Breno Alves <breno.alvesdx@gmail.com>
2025-03-15 01:42:07 -0300

    Feat: listagem de produtos funcionando

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -7,7 +7,6 @@
  *
  * @author Adm
  */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
@@ -15,17 +14,16 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement prep;
     conectaDAO c = new conectaDAO();
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-      try {
+
+    public void cadastrarProduto(ProdutosDTO produto) {
+        try {
 
             prep = c.connectDB().prepareStatement("INSERT INTO produtos(nome, valor, status) VALUES(?,?,?) ");
             prep.setString(1, produto.getNome());
@@ -38,14 +36,26 @@ public class ProdutosDAO {
             JOptionPane.showMessageDialog(null, "Não foi possível cadastrar!");
         }
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+        try {
+            prep = c.connectDB().prepareStatement("SELECT * FROM produtos");
+            resultset = prep.executeQuery();
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+                listagem.add(produto);
+            }
+            return listagem;
+
+        } catch (SQLException e) {
+
+            System.out.println("Erro! não foi possivel encontrar os produtos");
+        }
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}
